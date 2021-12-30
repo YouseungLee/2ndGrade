277413a5571f6832a7f03c530ec061b44ca38f91
youseunglee <snagg1377@gmail.com>
2021-12-31 01:25:20 +0900

    2021_12_31

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ img_index = 2;
 setInterval(function() {
     $("#img_img").attr("src", "./img/" + img_index + ".jpg");
     img_index++;
-    if (img_index == 7) {
+    if (img_index == 14) {
         img_index = 1;
     }
 }, 3000);
